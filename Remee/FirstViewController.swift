bc907b137b0184cb587abd3340805970aa022dc3
kazuki-yoshida <kzk.yshd@gmail.com>
2016-12-03 21:25:53 +0900

    Initial Commit

---
+++ Remee/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Remee
+//
+//  Created by kazuki yoshida on 2016/12/03.
+//  Copyright © 2016年 kazy.github.com. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
